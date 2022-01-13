48df5ecba576159064ea6022f2748e0a297c9bc9
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-13 16:00:47 +0300

    Proje dosyası ekle.

---
+++ InnerType/Araba.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnerType
+{
+    public class Araba
+    {
+        public string Marka;
+        public string Model;
+        public void InfoShow()
+        {
+            Marka = "Lamborghini";
+            Model = "Aventador";
+            Console.WriteLine("Arabanın Markası:"+Marka);
+            Console.WriteLine("Arabanın Modeli:" +Model);
+        }
+
+        public class ArabaIcDonanim //innertype
+        {
+            public string direksiyon;
+            public string viteskolu;
+            public void IcBilgi()
+            {
+                direksiyon = "Elektrikli Direksiyon";
+                viteskolu = "Tam Otomatik";
+                Console.WriteLine("Direksiyon Türü:"+direksiyon);
+                Console.WriteLine("Vites Kolu :" + viteskolu);
+
+            }
+        }
+    }
+}
---
+++ InnerType/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnerType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Araba bilgi = new Araba();
+            bilgi.InfoShow();
+
+            Araba.ArabaIcDonanim donanim = new Araba.ArabaIcDonanim();
+            donanim.IcBilgi();
+            Console.ReadKey();
+        }
+    }
+}
